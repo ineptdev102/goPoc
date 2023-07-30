fa9a1b0cd002affe289b42dd72168e00d320a92a
Akash chandra <akashchandra@Akash-Chandra.local>
2023-07-31 05:14:33 +0530

    change in both module

--- module/other/newPkg/go/main.go
+++ module/other/newPkg/go/main.go
@@ -5,3 +5,6 @@ import "log"
 func Logger2(s string) {
 	log.Printf("INFO: %s", s)
 }
+func LoggerWarn(s string) {
+	log.Printf("Warn: %s", s)
+}
--- module/sample/go/main.go
+++ module/sample/go/main.go
@@ -7,5 +7,5 @@ func Logger(s string) {
 }
 
 func LogFatal(s string) {
-	log.Printf("Fatal: %s", s)
+	log.Printf("Fatal_test: %s", s)
 }
